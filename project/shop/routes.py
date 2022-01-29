69cd9ed30bd80680b2bf7cb1e7c862fea5a0591c
Andrew Fisher <andrew.c.fisher97@gmail.com>
2022-01-29 15:10:32 -0500

    added forms, validations, and flashes

--- project/shop/routes.py
+++ project/shop/routes.py
@@ -1,6 +1,6 @@
-from shop import app
-from flask import render_template
-from shop.models import Item
+from shop import app, db
+from flask import render_template, redirect, url_for, flash, get_flashed_messages
+from shop.models import Item, User
 from shop.forms import RegisterForm
 
 @app.route('/')
@@ -13,7 +13,16 @@ def shop_page():
     items = Item.query.all()
     return render_template('shop.html', items=items)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data, email_address=form.email_address.data, password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('shop_page'))
+    if form.errors != {}: # if there are no errors from validations
+        for err_msg in form.errors.values():
+            flash(f'There was an error with account creation: {err_msg}', category='danger')
+
     return render_template('register.html', form=form)
