1192ba98237e6bc94252a91e526ac4d34c0f73c9
Andrew Fisher <andrew.c.fisher97@gmail.com>
2022-02-09 14:59:15 -0500

    Forms, Advanced Validations, User Authentication, Logout, Item Purchasing and Selling Added

--- project/shop/__init__.py
+++ project/shop/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
 app.config['SECRET_KEY'] = '72877a670ef4a289603df504'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 
 from shop import routes
